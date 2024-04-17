aa5736477714d0bf5bed0fd21254551e74572905
Jonas Engicht <dev@condecon.de>
2024-04-17 15:26:25 +0200

    remove comments and convert var to const

--- src/valid-email.ts
+++ src/valid-email.ts
@@ -1,38 +1,15 @@
-/*!
- * Valid Email
- * Copyright(c) 2013 John Henry
- * MIT Licensed
- */
-
-/**
- * Valid-Email:
- *
- * An alternative to using a regular expression to validate email.
- * Inspired by:
- *      http://stackoverflow.com/questions/997078/email-regular-expression
- *      http://stackoverflow.com/questions/201323/using-a-regular-expression-to-validate-an-email-address
- *
- * Examples:
- *     console.log(require('valid-email')('john@iamjohnhenry.com'))//#true
- *     console.log(require('valid-email')('iamjohnhenry.com'))//#false
- *
- * @param {String} email
- * // potential email address
- * @return {Boolean}
- * @api public
- */
 export function valid_email(email:string):Boolean {
   if (!email) {
     return false; // email empty or undefined
   }
-  var at = email.search("@");
+  const at = email.search("@");
   if (at < 0) {
     return false; // missing "@" symbol
   }
-  var user = email.substr(0, at);
-  var domain = email.substr(at + 1);
-  var userLen = user.length;
-  var domainLen = domain.length;
+  const user = email.substr(0, at);
+  const domain = email.substr(at + 1);
+  const userLen = user.length;
+  const domainLen = domain.length;
   if (userLen < 1 || userLen > 64) {
     return false; // user part length exceeded
   }
